b7abdd478d1f26d3f7d7c26c765bf0f0ae4def0f
Brian Sayre <briansayre01@gmail.com>
2020-03-04 10:38:42 -0600

    Added buttons to the home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div class="header"> 
-        QUAD <br/>
-        RATH <br />
-        ALON <br />
-        X <br />
+    <div>
+
+      <div class="header"> 
+          QUAD <br/>
+          RATH <br />
+          ALON <br />
+      </div>
+
+      <div class="button-flex-container">
+        <div class="row"> 
+          <div class="button-flex-item">
+            <button  class="pink-button"> LOG IN </button>
+          </div>
+    
+          <div class="button-flex-item">
+            <button  class="pink-button"> SIGN UP </button>
+          </div>
+    
+          <div class="button-flex-item">
+            <button  class="pink-button"> GUEST </button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }

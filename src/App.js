ef0ae39bbcad32adc29685ab83d311699c22324a
Brian Sayre <briansayre01@gmail.com>
2020-03-09 11:42:02 -0500

    Deleted unneeded files

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ function App() {
           ALON <br />
       </div>
 
+      <p class="home-text">
+        Test your gaming skills in <br/> a series of four games...
+      </p>
+      
       <div class="button-flex-container">
         <div class="row"> 
           <div class="button-flex-item">
-            <button  class="pink-button"> LOG IN </button>
+            <button class="pink-button"> LOG IN </button>
           </div>
     
           <div class="button-flex-item">
-            <button  class="pink-button"> SIGN UP </button>
+            <button class="pink-button"> SIGN UP </button>
           </div>
     
           <div class="button-flex-item">
-            <button  class="pink-button"> GUEST </button>
+            <button class="pink-button"> GUEST </button>
           </div>
         </div>
       </div>

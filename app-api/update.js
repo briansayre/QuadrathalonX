dde14db408805e472b0ae66fc2483a7517fd7526
briansayre <briansayre01@gmail.com>
2020-04-16 17:46:32 -0500

    Jackson and I did more work on the database

---
+++ app-api/update.js
@@ -0,0 +1,33 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET game1score = :game1score, game2score = :game2score, game3score = :game3score, game4score = :game4score, totalscore = :totalscore",
+    ExpressionAttributeValues: {
+      ":game1score": data.game1score || null,
+      ":game2score": data.game2score || null,
+      ":game3score": data.game3score || null,
+      ":game4score": data.game4score || null,
+      ":totalscore": data.totalscore || null,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
+
+  await dynamoDb.update(params);
+
+  return { status: true };
+});
